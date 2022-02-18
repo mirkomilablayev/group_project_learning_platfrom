43bc2e1484956c8807ce95e31b37e658bee1d588
mirkomilablayev <mirkomilablayev777@gmail.com>
2022-02-18 10:42:48 +0500

    Project is uploaded

---
+++ src/main/java/uz/pdp/Main.java
@@ -0,0 +1,14 @@
+package uz.pdp;
+/*
+ Group number name is B7
+ Mentor is Abror Ergashev
+ B7 is the best of the best
+*/
+
+//Author --  Ablayev Mirkomil 2/18/2022 --10:39 AM 
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
